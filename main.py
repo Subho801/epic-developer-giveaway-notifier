70b1aa32f5915f676642e840293b1ff0d689bd72
Arthur Morgan <lofibeatz95@gmail.com>
2026-08-05 00:36:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from src.config import COUNTRY
+from src.config import (
+    WEBHOOK_URL,
+    COUNTRY,
+    validate,
+)
+
 from src.epic import developer_giveaways
+
+from src.notifier import send_giveaway
 
 
 def main():
+    validate()
+
     games = developer_giveaways(COUNTRY)
 
     print(f"Found {len(games)} developer giveaways")
 
     for game in games:
-        print("-", game["title"])
+        print(game["title"])
+        send_giveaway(WEBHOOK_URL, game)
 
 
 if __name__ == "__main__":
